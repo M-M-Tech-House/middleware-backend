0ac3df4226e8b7595a864e32c1487828f4339ed1
Luis Miguel Morales Pajaro <luismorales@MacBook-Air-de-Luis-1815.local>
2026-01-02 21:52:59 -0500

    fix: db

---
+++ migrations/20260103025240-retry.js
@@ -0,0 +1,42 @@
+module.exports = {
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async up(db, client) {
+
+    // === sale_states ===
+    await db.collection("sale_states").insertMany([
+      { name: "Completada" },
+      { name: "Cancelada" },
+      { name: "Abierta" }
+    ]);
+
+    // === purchase_states ===
+    await db.collection("purchase_states").insertMany([
+      { name: "Pendiente" },
+      { name: "Aprobada" },
+      { name: "Recibida" },
+      { name: "Cancelada" }
+    ]);
+
+    // === pay_methods ===
+    await db.collection("pay_methods").insertMany([
+      { name: "Efectivo" },
+      { name: "Tarjeta Débito" }
+    ]);
+
+    console.log("✅ Datos insertados correctamente");
+
+  },
+
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible) ok
+  }
+};
